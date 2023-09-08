7a8c947836f566e19358344ec27a4ab4593ca508
CHAE <chaso1495@naver.com>
2023-09-09 01:00:56 +0900

    :tada: Init: first commit

---
+++ src/main/java/com/diareat/diareat/DiareatApplication.java
@@ -0,0 +1,13 @@
+package com.diareat.diareat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiareatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiareatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diareat/diareat/DiareatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diareat.diareat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiareatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
