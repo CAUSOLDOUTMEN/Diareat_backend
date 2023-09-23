6ea6541c251f76512fd8a57427c63c50634d3362
CHAE <chaso1495@naver.com>
2023-09-24 01:58:55 +0900

    :heavy_plus_sign: Chore: FoodService/Controller 의존성 추가 및 요구사항에 따른 메서드 작성 (#3)

--- src/main/java/com/diareat/diareat/food/controller/FoodController.java
+++ src/main/java/com/diareat/diareat/food/controller/FoodController.java
@@ -1,4 +1,9 @@
 package com.diareat.diareat.food.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class FoodController {
 }
