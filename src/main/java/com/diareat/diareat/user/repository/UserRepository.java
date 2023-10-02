fb1005e1bf89a503cd084326142d1f1d92584455
CHAE <chaso1495@naver.com>
2023-10-02 16:51:31 +0900

    ✨Feat: 기본적인 UserService 메서드 구축 (#7)

--- src/main/java/com/diareat/diareat/user/repository/UserRepository.java
+++ src/main/java/com/diareat/diareat/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.diareat.diareat.user.repository;
 import com.diareat.diareat.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByNameContaining(String name); // 회원이 팔로우를 위해 검색한 유저 목록 조회
 }
