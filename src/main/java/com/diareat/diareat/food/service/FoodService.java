fffacf3a27715d1f5dc9ee17091cea31984e7f71
Ahn Jiwan <synoti21@naver.com>
2023-10-04 20:23:00 +0900

    :sparkles: feat: 음식 저장 및 날짜별 음식 리스트 반환 (#9)

--- src/main/java/com/diareat/diareat/food/service/FoodService.java
+++ src/main/java/com/diareat/diareat/food/service/FoodService.java
@@ -1,9 +1,23 @@
 package com.diareat.diareat.food.service;
 
+import com.diareat.diareat.food.domain.Food;
+import com.diareat.diareat.food.dto.CreateFoodDto;
+import com.diareat.diareat.food.dto.ResponseFoodDto;
 import com.diareat.diareat.food.repository.FavoriteFoodRepository;
 import com.diareat.diareat.food.repository.FoodRepository;
+import com.diareat.diareat.user.domain.User;
+import com.diareat.diareat.user.dto.ResponseResearchUserDto;
+import com.diareat.diareat.user.repository.UserRepository;
+import com.diareat.diareat.util.api.ResponseCode;
+import com.diareat.diareat.util.exception.FoodException;
+import com.diareat.diareat.util.exception.UserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -12,10 +26,25 @@ public class FoodService {
     private final FoodRepository foodRepository; // 유저:음식은 1:다
     private final FavoriteFoodRepository favoriteFoodRepository; // 유저:즐찾음식은 1:다
 
+    private final UserRepository userRepository;
+
     // 촬영 후, 음식 정보 저장
-    public Long saveFood() {
-        return null;
+    @Transactional
+    public Long saveFood(CreateFoodDto createFoodDto) {
+        User user = userRepository.findById(createFoodDto.getUserId())
+                .orElseThrow(() -> new UserException(ResponseCode.USER_NOT_FOUND));
+        Food food = Food.createFood(createFoodDto.getName(), user, createFoodDto.getBaseNutrition());
+        return foodRepository.save(food).getId();
     }
+
+    // 회원이 특정 날짜에 먹은 음식 반환
+    @Transactional
+    public List<ResponseFoodDto> getFoodListByDate(Long userId, LocalDate date){
+        List<Food> foodList = foodRepository.findAllByUserIdAndDate(userId, date);
+        return foodList.stream()
+                .map(food -> ResponseFoodDto.of(food.getId(), food.getUser().getId(), food.getName(), food.getDate(), food.getTime(), food.getBaseNutrition(), food.isFavorite())).collect(Collectors.toList());
+    }
+
 
     // 음식 정보 수정
     public void updateFood() {
@@ -41,6 +70,7 @@ public class FoodService {
     public void deleteFavoriteFood() {
 
     }
+
 
     /**
      * 메서드 구현 유의사항
