6646330a31a90babbd0bf46104401a7944bc73ba
Ahn Jiwan <synoti21@naver.com>
2023-10-05 10:47:20 +0900

    :recycle: refactor: ResponseUserDto에 id 속성 제거 (#9)

--- src/main/java/com/diareat/diareat/user/dto/ResponseUserDto.java
+++ src/main/java/com/diareat/diareat/user/dto/ResponseUserDto.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ResponseUserDto {
 
-    private Long id; //누락된 id
     private String name;
     private int height;
     private int weight;
@@ -18,11 +17,11 @@ public class ResponseUserDto {
     private BaseNutrition baseNutrition;
 
 
-    public static ResponseUserDto of(Long id, String userName, int userHeight, int userWeight, int userGender, int userAge, BaseNutrition userBaseNutrition) {
-        return new ResponseUserDto(id, userName, userHeight, userWeight, userGender, userAge, userBaseNutrition);
+    public static ResponseUserDto of(String userName, int userHeight, int userWeight, int userGender, int userAge, BaseNutrition userBaseNutrition) {
+        return new ResponseUserDto(userName, userHeight, userWeight, userGender, userAge, userBaseNutrition);
     }
 
     public static ResponseUserDto from(User user) {
-        return new ResponseUserDto(user.getId(), user.getName(), user.getHeight(), user.getWeight(), user.getGender(), user.getAge(), user.getBaseNutrition());
+        return new ResponseUserDto(user.getName(), user.getHeight(), user.getWeight(), user.getGender(), user.getAge(), user.getBaseNutrition());
     }
 }
