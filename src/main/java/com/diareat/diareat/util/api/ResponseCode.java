3591486d37ce96b004d5f7184d3296000b1c1a07
CHAE <chaso1495@naver.com>
2023-10-13 11:53:41 +0900

    :sparkles: Feat: UserController 구현 및 ResponseCode 추가 (#22)

--- src/main/java/com/diareat/diareat/util/api/ResponseCode.java
+++ src/main/java/com/diareat/diareat/util/api/ResponseCode.java
@@ -16,15 +16,42 @@ public enum ResponseCode {
     FORBIDDEN(HttpStatus.FORBIDDEN, false, "권한이 없습니다."),
 
     // 404 Not Found
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, false,"사용자를 찾을 수 없습니다."),
-    FOOD_NOT_FOUND(HttpStatus.NOT_FOUND, false,"음식을 찾을 수 없습니다."),
-    FAVORITE_NOT_FOUND(HttpStatus.NOT_FOUND, false,"즐겨찾기를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, false, "사용자를 찾을 수 없습니다."),
+    FOOD_NOT_FOUND(HttpStatus.NOT_FOUND, false, "음식을 찾을 수 없습니다."),
+    FAVORITE_NOT_FOUND(HttpStatus.NOT_FOUND, false, "즐겨찾기를 찾을 수 없습니다."),
 
     // 405 Method Not Allowed
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, false,"허용되지 않은 메소드입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, false, "허용되지 않은 메소드입니다."),
+
+    // 409 Conflict
+    USER_ALREADY_EXIST(HttpStatus.CONFLICT, false, "이미 존재하는 닉네임입니다."),
+    FOLLOWED_ALREADY(HttpStatus.CONFLICT, false, "이미 팔로우한 사용자입니다."),
+    UNFOLLOWED_ALREADY(HttpStatus.CONFLICT, false, "이미 언팔로우한 사용자입니다."),
+    FOOD_ALREADY_EXIST(HttpStatus.CONFLICT, false, "이미 즐겨찾기에 존재하는 음식입니다."),
 
     // 500 Internal Server Error
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false,"서버에 오류가 발생하였습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "서버에 오류가 발생하였습니다."),
+
+    // 200 OK
+    USER_READ_SUCCESS(HttpStatus.CREATED, true, "사용자 정보 조회 성공"),
+    USER_UPDATE_SUCCESS(HttpStatus.OK, true, "사용자 정보 수정 성공"),
+    USER_SEARCH_SUCCESS(HttpStatus.OK, true, "사용자 검색 성공"),
+    USER_FOLLOW_SUCCESS(HttpStatus.OK, true, "사용자 팔로우 성공"),
+    USER_UNFOLLOW_SUCCESS(HttpStatus.OK, true, "사용자 언팔로우 성공"),
+
+    FOOD_READ_SUCCESS(HttpStatus.OK, true, "음식 정보 조회 성공"),
+    FOOD_UPDATE_SUCCESS(HttpStatus.OK, true, "음식 정보 수정 성공"),
+    FOOD_DELETE_SUCCESS(HttpStatus.OK, true, "음식 정보 삭제 성공"),
+    FOOD_FAVORITE_READ_SUCCESS(HttpStatus.OK, true, "즐겨찾기 음식 조회 성공"),
+    FOOD_FAVORITE_UPDATE_SUCCESS(HttpStatus.OK, true, "즐겨찾기 음식 수정 성공"),
+    FOOD_FAVORITE_DELETE_SUCCESS(HttpStatus.OK, true, "즐겨찾기 음식 삭제 성공"),
+
+
+    // 201 Created
+    USER_CREATE_SUCCESS(HttpStatus.CREATED, true, "사용자 생성 성공"),
+    FOOD_CREATE_SUCCESS(HttpStatus.CREATED, true, "음식 생성 성공"),
+    FOOD_FAVORITE_CREATE_SUCCESS(HttpStatus.OK, true, "즐겨찾기 음식 생성 성공");
+
 
     private final HttpStatus httpStatus;
     private final Boolean success;
@@ -33,4 +60,4 @@ public enum ResponseCode {
     public int getHttpStatusCode() {
         return httpStatus.value();
     }
-    }
+}
