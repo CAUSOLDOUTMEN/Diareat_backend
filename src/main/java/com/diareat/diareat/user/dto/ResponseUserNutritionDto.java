e7494e74d8ba1296b4e5680f68f1c6b12d5aba9b
CHAE <chaso1495@naver.com>
2023-10-17 02:21:49 +0900

    :sparkles: Feat: 유저에 type 속성 추가 및 기준섭취량 응답 dto 값 추가 (#35)

--- src/main/java/com/diareat/diareat/user/dto/ResponseUserNutritionDto.java
+++ src/main/java/com/diareat/diareat/user/dto/ResponseUserNutritionDto.java
@@ -8,16 +8,22 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ResponseUserNutritionDto {
 
-    private int calorie;
+    private int calorie; // 유저가 설정한(할 수 있는) 현재 영양소 기준섭취량
     private int carbohydrate;
     private int protein;
     private int fat;
 
-    public static ResponseUserNutritionDto of(int calorie, int carbohydrate, int protein, int fat) {
-        return new ResponseUserNutritionDto(calorie, carbohydrate, protein, fat);
+    private int defaultCalorie; // 개인정보에 따라 기본적으로 설정되는 영양소 기준섭취량
+    private int defaultCarbohydrate;
+    private int defaultProtein;
+    private int defaultFat;
+
+    public static ResponseUserNutritionDto of(int calorie, int carbohydrate, int protein, int fat, int defaultCalorie, int defaultCarbohydrate, int defaultProtein, int defaultFat) {
+        return new ResponseUserNutritionDto(calorie, carbohydrate, protein, fat, defaultCalorie, defaultCarbohydrate, defaultProtein, defaultFat);
     }
 
-    public static ResponseUserNutritionDto from(User user) {
-        return new ResponseUserNutritionDto(user.getBaseNutrition().getKcal(), user.getBaseNutrition().getCarbohydrate(), user.getBaseNutrition().getProtein(), user.getBaseNutrition().getFat());
+    public static ResponseUserNutritionDto from(User user, int defaultCalorie, int defaultCarbohydrate, int defaultProtein, int defaultFat) {
+        return new ResponseUserNutritionDto(user.getBaseNutrition().getKcal(), user.getBaseNutrition().getCarbohydrate(),
+                user.getBaseNutrition().getProtein(), user.getBaseNutrition().getFat(), defaultCalorie, defaultCarbohydrate, defaultProtein, defaultFat);
     }
 }
