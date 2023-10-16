229ac32904f6678ec8f7da0349fb27d63e45f41f
CHAE <chaso1495@naver.com>
2023-10-16 21:33:06 +0900

    :sparkles: Feat: Spring Security를 위한 User 객체에 UserDetails 인터페이스 상속  (#31)

---
+++ src/main/java/com/diareat/diareat/auth/service/CustomUserDetailService.java
@@ -0,0 +1,22 @@
+package com.diareat.diareat.auth.service;
+
+import com.diareat.diareat.user.repository.UserRepository;
+import com.diareat.diareat.util.api.ResponseCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String keyCode) throws UsernameNotFoundException {
+        return userRepository.findByKeyCode(keyCode)
+                .orElseThrow(() -> new UsernameNotFoundException(ResponseCode.USER_NOT_FOUND.getMessage()));
+    }
+}
