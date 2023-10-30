8c483142959088a2117385fb56d6b96996796464
CHAE <chaso1495@naver.com>
2023-10-30 19:59:39 +0900

    :sparkles: Feat: FoodService 잔여 기능 Dto 구축 (#42)

---
+++ src/main/java/com/diareat/diareat/food/dto/ResponseScoreBestWorstDto.java
@@ -0,0 +1,23 @@
+package com.diareat.diareat.food.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ResponseScoreBestWorstDto { // 일기 분석 자세히보기에 사용되는 DTO
+
+    private double calorieScore;
+    private double carbohydrateScore;
+    private double proteinScore;
+    private double fatScore;
+    private double totalScore;
+    private List<ResponseSimpleFoodDto> best;
+    private List<ResponseSimpleFoodDto> worst;
+
+    public static ResponseScoreBestWorstDto of(double calorieScore, double carbohydrateScore, double proteinScore, double fatScore, double totalScore, List<ResponseSimpleFoodDto> best, List<ResponseSimpleFoodDto> worst) {
+        return new ResponseScoreBestWorstDto(calorieScore, carbohydrateScore, proteinScore, fatScore, totalScore, best, worst);
+    }
+}
