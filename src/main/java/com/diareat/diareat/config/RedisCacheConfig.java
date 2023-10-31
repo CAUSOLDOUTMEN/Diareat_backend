462c8508e4fc71160cb0c88cdc593802086f0d9b
CHAE <chaso1495@naver.com>
2023-11-01 00:31:23 +0900

    :sparkles: Feat: EnableCaching 어노테이션 도입 (#44)

--- src/main/java/com/diareat/diareat/config/RedisCacheConfig.java
+++ src/main/java/com/diareat/diareat/config/RedisCacheConfig.java
@@ -1,7 +1,6 @@
 package com.diareat.diareat.config;
 
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -14,7 +13,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @Configuration
-@EnableCaching
 public class RedisCacheConfig { // Redis Cache 설정
 
     @Bean
