369d9d2488e401b3802e2f43fd7b7349673f1b4c
Ahn Jiwan <synoti21@naver.com>
2023-11-06 19:31:18 +0900

    :sparkles: feat: 잔여 기능 (식습관 점수, best & worst, 그래프, 랭킹) api 구현 (#50)

--- src/main/java/com/diareat/diareat/food/controller/FoodController.java
+++ src/main/java/com/diareat/diareat/food/controller/FoodController.java
@@ -2,10 +2,12 @@ package com.diareat.diareat.food.controller;
 
 import com.diareat.diareat.food.dto.*;
 import com.diareat.diareat.food.service.FoodService;
+import com.diareat.diareat.user.dto.response.ResponseRankUserDto;
 import com.diareat.diareat.util.api.ApiResponse;
 import com.diareat.diareat.util.api.ResponseCode;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -93,23 +95,42 @@ public class FoodController {
                                                                             @RequestParam int mm,
                                                                             @RequestParam int dd){
         LocalDate date = LocalDate.of(yy,mm,dd);
-        return ApiResponse.success(foodService.getNutritionSumByDate(userId,date),ResponseCode.FOOD_FAVORITE_READ_SUCCESS.getMessage());
+        return ApiResponse.success(foodService.getNutritionSumByDate(userId,date),ResponseCode.FOOD_READ_SUCCESS.getMessage());
     }
 
     //"" 7일간 총합 조회
     @Operation(summary = "[음식] 최근 7일간 먹은 음식들의 영양성분 총합 조회",description = "최근 7일 간 유저가 먹은 음식들의 영양성분별 총합 및 권장섭취량에 대한 비율을 조회합니다.")
     @GetMapping("/{userId}/nutrition/recentWeek")
     public ApiResponse<ResponseNutritionSumByDateDto> getNutritionSumByWeek(@PathVariable Long userId){
-        return ApiResponse.success(foodService.getNutritionSumByWeek(userId),ResponseCode.FOOD_FAVORITE_READ_SUCCESS.getMessage());
+        return ApiResponse.success(foodService.getNutritionSumByWeek(userId),ResponseCode.FOOD_READ_SUCCESS.getMessage());
     }
 
     //"" 30일간 (1달간) 총합 조회
     @Operation(summary = "[음식] 최근 한달 간 먹은 음식들의 영양성분 총합 조회",description = "최근 한달 간 유저가 먹은 음식들의 영양성분별 총합 및 권장섭취량에 대한 비율을 조회합니다.")
     @GetMapping("/{userId}/nutrition/recentMonth")
     public ApiResponse<ResponseNutritionSumByDateDto> getNutritionSumByMonth(@PathVariable Long userId){
-        return ApiResponse.success(foodService.getNutritionSumByMonth(userId),ResponseCode.FOOD_FAVORITE_READ_SUCCESS.getMessage());
+        return ApiResponse.success(foodService.getNutritionSumByMonth(userId),ResponseCode.FOOD_READ_SUCCESS.getMessage());
 
     }
 
+    //유저의 주간 식습관 점수와 best3, worst3 음식 조회
+    @Operation(summary = "[음식] 유저의 주간 식습관 점수와 best3, worst3 음식 조회",description = "유저의 주간 식습관 점수와 best3, worst3 음식을 조회합니다.")
+    @GetMapping("/{userId}/score")
+    public ApiResponse<ResponseScoreBestWorstDto> getScoreOfUserWithBestAndWorstFoods(@PathVariable Long userId){
+        return ApiResponse.success(foodService.getScoreOfUserWithBestAndWorstFoods(userId),ResponseCode.FOOD_READ_SUCCESS.getMessage());
+    }
 
+    //유저의 일기 분석 그래프 데이터 및 식습관 totalScore 조회
+    @Operation(summary = "[음식] 유저의 일기 분석 그래프 데이터 및 주간 식습관 점수 조회",description = "유저의 일기 분석 그래프 데이터 및 식습관 점수를 조회합니다.")
+    @GetMapping("/{userId}/analysis")
+    public ApiResponse<ResponseAnalysisDto> getAnalysisOfUser(@PathVariable Long userId){
+        return ApiResponse.success(foodService.getAnalysisOfUser(userId),ResponseCode.FOOD_READ_SUCCESS.getMessage());
+    }
+
+    //유저의 식습관 점수를 기반으로 한 주간 랭킹 조회
+    @Operation(summary = "[음식] 유저의 식습관 점수를 기반으로 한 주간 랭킹 조회",description = "유저의 식습관 점수를 기반으로 한 주간 랭킹을 조회합니다. (팔로잉 상대 기반)")
+    @GetMapping("/{userId}/rank")
+    public ApiResponse<List<ResponseRankUserDto>> getUserRankByWeek(@PathVariable Long userId){
+        return ApiResponse.success(foodService.getUserRankByWeek(userId),ResponseCode.FOOD_RANK_READ_SUCCESS.getMessage());
+    }
 }
