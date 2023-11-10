20103b6252dbdeeeb54b1428a4d9cc6b0c666452
CHAE <chaso1495@naver.com>
2023-11-10 14:09:15 +0900

    :ambulance: Fix: 오늘의영양 응답데이터 LocalDate 주석처리 (#66)

--- src/main/java/com/diareat/diareat/food/dto/ResponseNutritionSumByDateDto.java
+++ src/main/java/com/diareat/diareat/food/dto/ResponseNutritionSumByDateDto.java
@@ -3,15 +3,18 @@ package com.diareat.diareat.food.dto;
 import com.diareat.diareat.user.domain.BaseNutrition;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
-public class ResponseNutritionSumByDateDto {
+public class ResponseNutritionSumByDateDto implements Serializable {
 
     Long userId;
-    LocalDate checkDate; //조회한 날짜
+    // LocalDate checkDate; //조회한 날짜
     int nutritionSumType; //조회할 기간을 나타내는 코드. {1: 특정 날짜, 7: 최근 7일간, 30: 최근 한달간}
 
     int totalKcal;
@@ -30,7 +33,7 @@ public class ResponseNutritionSumByDateDto {
                                                     int totalCarbohydrate, int totalProtein, int totalFat, double ratioKcal,
                                                     double ratioCarbohydrate, double ratioProtein, double ratioFat,
                                                     BaseNutrition baseNutrition){
-        return new ResponseNutritionSumByDateDto(userId, checkDate, nutritionSumType, totalKcal,
+        return new ResponseNutritionSumByDateDto(userId, nutritionSumType, totalKcal,
                                                 totalCarbohydrate, totalProtein, totalFat, ratioKcal,
                                                 ratioCarbohydrate, ratioProtein, ratioFat, baseNutrition);
     }
