c352d8d6dc5db434efef339864d0ff433e895ae3
CHAE <chaso1495@naver.com>
2023-11-10 15:08:43 +0900

    :ambulance: Fix: Redis 포트번호 미정 문제 수정 (#66)

--- src/main/java/com/diareat/diareat/config/RedisCacheConfig.java
+++ src/main/java/com/diareat/diareat/config/RedisCacheConfig.java
@@ -1,6 +1,8 @@
 package com.diareat.diareat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,8 +14,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
+@EnableCaching
 @Configuration
 public class RedisCacheConfig { // Redis Cache 설정
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
 
     @Bean
     public CacheManager diareatCacheManager(RedisConnectionFactory cf) {
