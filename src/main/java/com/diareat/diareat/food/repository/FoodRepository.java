078f9b9fcd77f79071c9a966de71f9fc157b8403
Ahn Jiwan <synoti21@naver.com>
2023-11-12 02:01:04 +0900

    :sparkles: feat: 생성 날짜별 정렬 추가 (#72)

--- src/main/java/com/diareat/diareat/food/repository/FoodRepository.java
+++ src/main/java/com/diareat/diareat/food/repository/FoodRepository.java
@@ -1,6 +1,7 @@
 package com.diareat.diareat.food.repository;
 
 import com.diareat.diareat.food.domain.Food;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -9,6 +10,6 @@ import java.util.List;
 public interface FoodRepository extends JpaRepository<Food, Long> {
     boolean existsByIdAndUserId(Long id, Long userId); // 유저가 먹은 음식인지 확인
     boolean existsByName(String name);
-    List<Food> findAllByUserIdAndDate(Long userId, LocalDate date); //유저가 특정 날짜에 먹은 음식 반환
-    List<Food> findAllByUserIdAndDateBetween(Long userId, LocalDate startDate, LocalDate endDate); // 유저가 특정 기간 내에 먹은 음식 반환
+    List<Food> findAllByUserIdAndDate(Long userId, LocalDate date, Sort sort); //유저가 특정 날짜에 먹은 음식 반환
+    List<Food> findAllByUserIdAndDateBetween(Long userId, LocalDate startDate, LocalDate endDate, Sort sort); // 유저가 특정 기간 내에 먹은 음식 반환
 }
