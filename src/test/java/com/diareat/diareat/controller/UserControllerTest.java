b38aaf65db8f1c24a43d249185b2d0e6ae874d57
CHAE <chaso1495@naver.com>
2024-01-22 16:36:06 +0900

    :white_check_mark: Refactor: FoodControllerTest 개편사항 반영 및 UserControllerTest 일부 수정 (#145)

--- src/test/java/com/diareat/diareat/controller/UserControllerTest.java
+++ src/test/java/com/diareat/diareat/controller/UserControllerTest.java
@@ -66,6 +66,8 @@ class UserControllerTest {
     void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         testUser.setId(testUserId);
+        when(jwtTokenProvider.validateToken(any(String.class))).thenReturn(true);
+        when(jwtTokenProvider.getUserPk(any(String.class))).thenReturn(testUserId);
         when(userService.getSimpleUserInfo(any(Long.class)))
                 .thenReturn(ResponseSimpleUserDto.builder()
                         .name(testUser.getName())
@@ -95,7 +97,7 @@ class UserControllerTest {
         // When & Then
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/api/user/info/simple")
-                        .header("accessToken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3MDQ0MzgxMDQsImV4cCI6MTczNTk3NDEwNCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.ApqSkkS8DqNG_H9abjJMRrzxONDj59cVGugJxPM_nBg")
+                        .header("accessToken", "test")
                         .accept(MediaType.APPLICATION_JSON)
                         .with(authentication(authentication)))
                 .andExpect(MockMvcResultMatchers.status().isOk())
